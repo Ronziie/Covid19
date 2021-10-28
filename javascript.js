3706edb5181e599124add4f78c59010845a25d07
Ronaldo Dias <ronaldo@Ronaldos-MacBook-Air.local>
2021-10-28 19:10:39 +0100

    respond iphone

--- javascript.js
+++ javascript.js
@@ -3,72 +3,40 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-})
+});
 
-
-(function() {
+(function(){
     "use strict";
-  
+
     const select = (el, all = false) => {
-      el = el.trim()
-      if (all) {
-        return [...document.querySelectorAll(el)]
-      } else {
-        return document.querySelector(el)
-      }
-    }
-  
-   
-    const on = (type, el, listener, all = false) => {
-      let selectEl = select(el, all)
-      if (selectEl) {
+        el = el.trim()
         if (all) {
-          selectEl.forEach(e => e.addEventListener(type, listener))
+            return [...document.querySelectorAll(el)]
         } else {
-          selectEl.addEventListener(type, listener)
+            return document.querySelector(el)
         }
-      }
     }
-  
-    /**
-     * Easy on scroll event listener 
-     */
+
     const onscroll = (el, listener) => {
-      el.addEventListener('scroll', listener)
-    }
-  
-    
-  
-    /**
-     * Back to top button
-     */
+        el.addEventListener('scroll', listener)
+      }
+      
+    /** back to top */
     let backtotop = select('.back-to-top')
     if (backtotop) {
-      const toggleBacktotop = () => {
-        if (window.scrollY > 100) {
-          backtotop.classList.add('active')
-        } else {
-          backtotop.classList.remove('active')
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
         }
-      }
-      window.addEventListener('load', toggleBacktotop)
-      onscroll(document, toggleBacktotop)
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
     }
- 
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-      AOS.init({
-        duration: 1000,
-        easing: 'ease-in-out',
-        once: true,
-        mirror: false
-      })
-    });
-  
-  })();
-  
-  
-  
-  
+
+    
+    
+})();
+
+
